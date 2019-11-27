853b7ea030d4039d4c791b0d2af697fb98a0ab30
Bui Quang Toan <toan.bui@asiantech.vn>
2019-11-27 15:00:03 +0700

    update some testcase

---
+++ Test Suites/Healthcare Login/Healthcare Login.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Healthcare Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>24d67f8f-5d4a-4101-9e98-1c107bf6166a</testSuiteGuid>
+   <testCaseLink>
+      <guid>3a3cfb03-d67f-431c-84b8-f973a6d0a2c3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/KatalonHealthcare/TC01_Move to Login page</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
