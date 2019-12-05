19c77d782499fc4a95238dbe248c75848550defd
mba0073 <mba0073@192.168.1.114>
2019-12-05 23:31:06 +0700

    Commit_Practice_1_Edit

--- Scripts/Practice_TestCase_1/Script1575561784632.groovy
+++ Scripts/Practice_TestCase_1/Script1575561784632.groovy
@@ -18,14 +18,13 @@ import org.openqa.selenium.Keys as Keys
 
 WebUI.openBrowser('')
 
-WebUI.navigateToUrl('https://www.phptravels.net/admin')
+WebUI.navigateToUrl('https://www.phptravels.net/admin/settings/')
 
 WebUI.setText(findTestObject('Practice_1/Page_Administator Login/text_Login Panel_email'), 'admin@phptravels.com')
 
 WebUI.click(findTestObject('Object Repository/Practice_1/Page_Administator Login/span_Password'))
 
-WebUI.setEncryptedText(findTestObject('Practice_1/Page_Administator Login/txt_Email Address_password'), 
-    'orSGNCvhf+w8SKCExcig5g==')
+WebUI.setEncryptedText(findTestObject('Practice_1/Page_Administator Login/txt_Email Address_password'), 'orSGNCvhf+w8SKCExcig5g==')
 
 WebUI.click(findTestObject('Practice_1/Page_Administator Login/btn_Login'))
 
@@ -41,62 +40,47 @@ WebUI.setText(findTestObject('Practice_1/Page_Application Settings/txt_License K
 
 WebUI.setText(findTestObject('Practice_1/Page_Application Settings/txt_Copyrights_copyright'), 'All Rights Reserved by YourCompany')
 
-WebUI.selectOptionByValue(findTestObject('Practice_1/Page_Application Settings/ddl_ddmmyyyy mmddyyyy'), 
-    'm/d/Y', true)
+WebUI.selectOptionByValue(findTestObject('Practice_1/Page_Application Settings/ddl_ddmmyyyy mmddyyyy'), 'm/d/Y', true)
 
-WebUI.selectOptionByValue(findTestObject('Practice_1/Page_Application Settings/ddl_Enabled Disabled'), 
-    '0', true)
+WebUI.selectOptionByValue(findTestObject('Practice_1/Page_Application Settings/ddl_Enabled Disabled'), '0', true)
 
-WebUI.selectOptionByValue(findTestObject('Practice_1/Page_Application Settings/ddl_Russian _7e6470'), 
-    'vi', true)
+WebUI.selectOptionByValue(findTestObject('Practice_1/Page_Application Settings/ddl_Russian _7e6470'), 'vi', true)
 
-WebUI.selectOptionByValue(findTestObject('Practice_1/Page_Application Settings/ddl_Enable Disable'), 
-    '0', true)
+WebUI.selectOptionByValue(findTestObject('Practice_1/Page_Application Settings/ddl_Enable Disable'), '0', true)
 
-WebUI.selectOptionByValue(findTestObject('Practice_1/Page_Application Settings/ddl_No Yes'), 
-    'Yes', true)
+WebUI.selectOptionByValue(findTestObject('Practice_1/Page_Application Settings/ddl_No Yes'), 'Yes', true)
 
-WebUI.selectOptionByValue(findTestObject('Practice_1/Page_Application Settings/ddl_Yes No'), 
-    '0', true)
+WebUI.selectOptionByValue(findTestObject('Practice_1/Page_Application Settings/ddl_Yes No'), '0', true)
 
-WebUI.selectOptionByValue(findTestObject('Practice_1/Page_Application Settings/ddl_Auto Approve Admin Approve'), 
-    'Yes', true)
+WebUI.selectOptionByValue(findTestObject('Practice_1/Page_Application Settings/ddl_Auto Approve Admin Approve'), 'Yes', 
+    true)
 
-WebUI.setText(findTestObject('Practice_1/Page_Application Settings/txt_Booking Expiry_bookingexpiry'), 
-    '10')
+WebUI.setText(findTestObject('Practice_1/Page_Application Settings/txt_Booking Expiry_bookingexpiry'), '10')
 
-WebUI.selectOptionByValue(findTestObject('Practice_1/Page_Application Settings/ddl_Alphabets Only Numbe_35158d'), 
-    'alnum', true)
+WebUI.selectOptionByValue(findTestObject('Practice_1/Page_Application Settings/ddl_Alphabets Only Numbe_35158d'), 'alnum', 
+    true)
 
-WebUI.setText(findTestObject('Practice_1/Page_Application Settings/txt_Coupon code Length_codelength'), 
-    '8')
+WebUI.setText(findTestObject('Practice_1/Page_Application Settings/txt_Coupon code Length_codelength'), '8')
 
-WebUI.selectOptionByValue(findTestObject('Practice_1/Page_Application Settings/ddl_Yes No_1'), 
-    '1', true)
+WebUI.selectOptionByValue(findTestObject('Practice_1/Page_Application Settings/ddl_Yes No_1'), '1', true)
 
-WebUI.setText(findTestObject('Practice_1/Page_Application Settings/txa_Our website is offline currently'), 
-    'This is the offline message')
+WebUI.setText(findTestObject('Practice_1/Page_Application Settings/txa_Our website is offline currently'), 'This is the offline message')
 
 WebUI.click(findTestObject('Practice_1/Page_Application Settings/txt_Home Title_slogan'))
 
 WebUI.setText(findTestObject('Practice_1/Page_Application Settings/txt_Home Title_slogan'), 'YourCompany | The Future')
 
-WebUI.setText(findTestObject('Practice_1/Page_Application Settings/txt_Default Keywords_keywords'), 
-    'This is my default keywords')
+WebUI.setText(findTestObject('Practice_1/Page_Application Settings/txt_Default Keywords_keywords'), 'This is my default keywords')
 
-WebUI.setText(findTestObject('Practice_1/Page_Application Settings/txa_php travels is complete booking an_a50366'), 
-    'This is my default description')
+WebUI.setText(findTestObject('Practice_1/Page_Application Settings/txa_php travels is complete booking an_a50366'), 'This is my default description')
 
 WebUI.setText(findTestObject('Practice_1/Page_Application Settings/txt_Google Map API_mapapi'), 'http://endpoint-s3.api.awsamazon.com')
 
-WebUI.setText(findTestObject('Practice_1/Page_Application Settings/txa_Tracking Analytics_gacode'), 
-    'Tracking & Analytics code')
+WebUI.setText(findTestObject('Practice_1/Page_Application Settings/txa_Tracking Analytics_gacode'), 'Tracking & Analytics code')
 
-WebUI.selectOptionByValue(findTestObject('Practice_1/Page_Application Settings/ddl_Enabled Disabled_1'), 
-    '0', true)
+WebUI.selectOptionByValue(findTestObject('Practice_1/Page_Application Settings/ddl_Enabled Disabled_1'), '0', true)
 
-WebUI.selectOptionByValue(findTestObject('Practice_1/Page_Application Settings/ddl_Every 1 Day _5906a9'), 
-    '0', true)
+WebUI.selectOptionByValue(findTestObject('Practice_1/Page_Application Settings/ddl_Every 1 Day _5906a9'), '0', true)
 
 WebUI.click(findTestObject('Practice_1/Page_Application Settings/btn_Submit'))
 
