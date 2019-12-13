40c08f2aa3bda7320c71d98a709a38ce7d05856a
at-phannguyen <phan.nguyen@asiantech.vn>
2019-12-13 23:05:24 +0700

    Update homework of lesson 2

--- Scripts/Lesson2/PhanNguyen_Day2_Exercise05/Script1576114479537.groovy
+++ Scripts/Lesson2/PhanNguyen_Day2_Exercise05/Script1576114479537.groovy
@@ -1,28 +1,8 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import internal.GlobalVariable as GlobalVariable
 
 WebUI.openBrowser('https://www.seleniumeasy.com/test/basic-select-dropdown-demo.html')
-
-CountOption = WebUI.getNumberOfTotalOption(findTestObject('Lesson2/Exercise-05/Page_Selenium Easy Demo - Automate All Scenarios/ddlSelectADay'))
-
-WebUI.verifyEqual(CountOption, 8)
-
-not_run: WebUI.verifyOptionPresentByValue(findTestObject('Lesson2/Exercise-05/Page_Selenium Easy Demo - Automate All Scenarios/ddlSelectADay'), 
-    'Sunday', false, 0)
 
 not_run: WebUI.verifyOptionPresentByValue(findTestObject('Lesson2/Exercise-05/Page_Selenium Easy Demo - Automate All Scenarios/ddlSelectADay'), 
     'Monday', false, 0)
@@ -41,6 +21,13 @@ not_run: WebUI.verifyOptionPresentByValue(findTestObject('Lesson2/Exercise-05/Pa
 
 not_run: WebUI.verifyOptionPresentByValue(findTestObject('Lesson2/Exercise-05/Page_Selenium Easy Demo - Automate All Scenarios/ddlSelectADay'), 
     'Saturday', false, 0)
+
+not_run: WebUI.verifyOptionPresentByValue(findTestObject('Lesson2/Exercise-05/Page_Selenium Easy Demo - Automate All Scenarios/ddlSelectADay'), 
+    'Sunday', false, 0)
+
+CountOption = WebUI.getNumberOfTotalOption(findTestObject('Lesson2/Exercise-05/Page_Selenium Easy Demo - Automate All Scenarios/ddlSelectADay'))
+
+WebUI.verifyEqual(CountOption, 8)
 
 WebUI.selectOptionByIndex(findTestObject('Lesson2/Exercise-05/Page_Selenium Easy Demo - Automate All Scenarios/ddlSelectADay'), 
     '2')
