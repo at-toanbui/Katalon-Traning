cac387417ad7641ac3ee63572f2a0fafcfef5da5
at-phannguyen <phan.nguyen@asiantech.vn>
2019-12-13 09:41:28 +0700

    Update homework of lesson 2

--- Scripts/Lesson2/PhanNguyen_Day2_Exercise02/Script1576202028890.groovy
+++ Scripts/Lesson2/PhanNguyen_Day2_Exercise02/Script1576202028890.groovy
@@ -22,7 +22,7 @@ WebUI.setText(findTestObject('Lesson2/Exercise-02-12/Page_Katalon Shop  Katalon 
 
 WebUI.sendKeys(findTestObject('Lesson2/Exercise-02-12/Page_Katalon Shop  Katalon Ecommerce/txtSearch'), Keys.chord(Keys.ENTER))
 
-WebUI.verifyElementVisible(findTestObject('Lesson2/Exercise-02-12/Page_Search Results for Woo Single 2  Katalon Shop/lblWooSingle2'))
+WebUI.waitForPageLoad(3)
 
 PageTitle = WebUI.getWindowTitle()
 
