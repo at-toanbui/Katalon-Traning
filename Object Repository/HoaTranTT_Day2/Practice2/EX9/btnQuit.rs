93a6bd1c33fffa35a161227bf04f80a1e4d35526
at-hoatran <hoa.tran@asiantech.vn>
2019-12-14 12:21:26 +0700

    Hoa_D2_Prac2_Full

---
+++ Object Repository/HoaTranTT_Day2/Practice2/EX9/btnQuit.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btnQuit</name>
+   <tag></tag>
+   <elementGuidId>e83ac58a-d4e7-414b-9580-290f649f47f3</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//span[text()='Quit']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
--- Scripts/HoaTranTT_Day2/HoaTranTT_Day2_Practice2_Ex9/Script1576130909727.groovy
+++ Scripts/HoaTranTT_Day2/HoaTranTT_Day2_Practice2_Ex9/Script1576130909727.groovy
@@ -21,7 +21,9 @@ WebUI.navigateToUrl('http://demo.guru99.com/test/simple_context_menu.html')
 
 WebUI.rightClick(findTestObject('HoaTranTT_Day2/Practice2/EX9/btnRightClickMe'))
 
-WebUI.selectOptionByIndex(findTestObject('HoaTranTT_Day2/Practice2/EX9/btnRightClickMe'), '6')
+WebUI.click(findTestObject('HoaTranTT_Day2/Practice2/EX9/btnQuit'), FailureHandling.CONTINUE_ON_FAILURE)
+
+WebUI.dismissAlert()
 
 WebUI.closeBrowser()
 
