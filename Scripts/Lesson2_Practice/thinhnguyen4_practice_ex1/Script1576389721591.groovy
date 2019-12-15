94fca953d4c490f0ed645d8f77c1784285de0b80
at-thinhnguyen4 <thinh.nguyen4@asiantech.vn>
2019-12-15 14:00:08 +0700

    Commit

---
+++ Scripts/Lesson2_Practice/thinhnguyen4_practice_ex1/Script1576389721591.groovy
@@ -0,0 +1,29 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('http://cms.demo.katalon.com/')
+
+WebUI.click(findTestObject('Lesson 2/Page_Katalon Shop  Katalon Ecommerce/btn_Sample Page'))
+
+WebUI.click(findTestObject('Lesson 2/Page_Sample Page  Katalon Shop/btn_WordPressorg'))
+
+url = WebUI.getUrl()
+
+WebUI.verifyEqual(url, 'https://wordpress.org/')
+
