1d97d4bcfb8d10850bdeba6f83bc3d5d15828ad4
Hanh Nguyen T <hanh.nguyen@asiantech.vn>
2019-12-18 17:12:07 +0700

    test suiltes and collection test suilt

---
+++ Test Suites/Browser.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Browser</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>c6a00263-5c6a-45aa-8501-53b6177df10a</testSuiteGuid>
+   <testCaseLink>
+      <guid>59cd21ef-19f6-4739-b490-8361dea6bcb8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/HanhNguyenT_Day2_EX-01</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>aabbfce4-59a9-41da-9ca4-861398814881</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/HanhNguyenT_Day2_EX-02</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
