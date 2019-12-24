2e1eee1d69775c850eda084422607bc2f92d84d8
at-thaile <thai.le2@asiantech.vn>
2019-12-24 10:10:46 +0700

    fix

---
+++ Object Repository/EX-02/Page_My Store/txtNoResult.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>txtNoResult</name>
+   <tag></tag>
+   <elementGuidId>ae286c56-ebf8-4a3f-844e-9b6da1e726ae</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id=&quot;center_column&quot;]/p[@class=&quot;alert alert-warning&quot;]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Scripts/Lesson3/ThaiLeM-Day3-EX02/Script1577150347284.groovy
@@ -0,0 +1,93 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('http://automationpractice.com/index.php')
+
+WebUI.setText(findTestObject('EX-02/Page_My Store/txtSearch'), 'Dress')
+
+WebUI.click(findTestObject('EX-02/Page_My Store/btnSearch'))
+
+WebUI.click(findTestObject('EX-02/Page_My Store/imgFirst'))
+
+WebUI.click(findTestObject('EX-02/Page_Printed Summer Dress - My Store/btnIconplus'))
+
+WebUI.selectOptionByLabel(findTestObject('EX-02/Page_Printed Summer Dress - My Store/ddlSize'), 'S', false)
+
+WebUI.click(findTestObject('EX-02/Page_Printed Summer Dress - My Store/btnBlue'))
+
+WebUI.click(findTestObject('EX-02/Page_Printed Summer Dress - My Store/btnAddtocart'))
+
+String totalProduct = WebUI.getText(findTestObject('EX-02/Page_Printed Summer Dress - My Store (1)/lblTotalP'))
+
+totalProduct = totalProduct.replaceAll('[$,]', '')
+
+double a = Double.valueOf(totalProduct)
+
+String totalShipping = WebUI.getText(findTestObject('EX-02/Page_Printed Summer Dress - My Store (1)/lblTotalS'))
+
+totalShipping = totalShipping.replaceAll('[$,]', '')
+
+double b = Double.valueOf(totalShipping)
+
+String total = WebUI.getText(findTestObject('EX-02/Page_Printed Summer Dress - My Store (1)/lblTotal'))
+
+total = total.replaceAll('[$,]', '')
+
+double c = Double.valueOf(total)
+
+String totalExpected =  a + b
+
+WebUI.verifyEqual(total, totalExpected)
+
+WebUI.click(findTestObject('EX-02/Page_Printed Summer Dress - My Store/btnProceedcheckout'))
+
+quantity = WebUI.getAttribute(findTestObject('EX-02/Page_Order - My Store (1)/txtQuantity'), 'value')
+
+WebUI.verifyEqual(quantity, '2', FailureHandling.STOP_ON_FAILURE)
+
+String currentTotal = WebUI.getText(findTestObject('EX-02/Page_Order - My Store (1)/txtTotalF'))
+
+currentTotal = currentTotal.replaceAll('[$,]', '')
+
+WebUI.verifyEqual(total, currentTotal)
+
+WebUI.click(findTestObject('EX-02/Page_Order - My Store/btnProceedcheckout2'))
+
+WebUI.setText(findTestObject('EX-02/Page_Login - My Store/txtEmail'), 'minhthai@gmail.com')
+
+WebUI.setText(findTestObject('EX-02/Page_Login - My Store/txtPassword'), '123456')
+
+WebUI.click(findTestObject('EX-02/Page_Login - My Store/btnSignin'))
+
+WebUI.click(findTestObject('EX-02/Page_My Store/chkDeliveryAddress'))
+
+WebUI.click(findTestObject('EX-02/Page_Order - My Store/btnProceedcheckout3'))
+
+WebUI.click(findTestObject('EX-02/Page_My Store/chkAgree'))
+
+WebUI.click(findTestObject('EX-02/Page_Order - My Store/btnProceedcheckout4'))
+
+WebUI.click(findTestObject('EX-02/Page_Order - My Store/btnPaybycheck'))
+
+WebUI.click(findTestObject('EX-02/Page_My Store/btnConfirm'))
+
+WebUI.verifyTextPresent('Your order on My Store is complete.', true, FailureHandling.STOP_ON_FAILURE)
+
+WebUI.closeBrowser()
+
