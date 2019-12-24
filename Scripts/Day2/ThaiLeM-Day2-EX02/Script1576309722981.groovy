8e9c74c0e3e432c68ae5304489c57ea8f9e714fa
at-thaile <thai.le2@asiantech.vn>
2019-12-24 10:22:04 +0700

    test suit collection

---
+++ Scripts/Day2/ThaiLeM-Day2-EX02/Script1576309722981.groovy
@@ -0,0 +1,39 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('http://cms.demo.katalon.com/')
+
+WebUI.setText(findTestObject('Day2/EX-01-02/Page_Katalon Shop  Katalon Ecommerce/txtSearch'), 'Woo Single #2')
+
+WebUI.click(findTestObject('Day2/EX-01-02/Page_Katalon Shop  Katalon Ecommerce/btnSearch'))
+
+String title = WebUI.getWindowTitle()
+
+String title2 = '“Woo Single #2” – Katalon Shop'
+
+if (title.contains(title2)) 
+{
+    WebUI.comment('Title matched')
+} 
+
+else
+    WebUI.executeJavascript(alert('Title not matched'), null)
+
+WebUI.closeBrowser()
+
---
+++ Test Suites/Element.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Element</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>1f70e69e-e8e2-4caa-b92c-417652ba0330</testSuiteGuid>
+   <testCaseLink>
+      <guid>f52f42ca-c1ac-4003-b220-ec3680f61b44</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Day2/ThaiLeM-Day2-EX04</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f94ee414-75ba-4796-b965-d8740ac344ee</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Day2/ThaiLeM-Day2-EX05</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b9f3fecc-c9e8-468a-9ef2-166cd8b0a1a6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Day2/ThaiLeM-Day2-EX03</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
